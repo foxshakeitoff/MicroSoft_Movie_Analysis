3fffd70b27c9e9ed2fff138c44405f78e5caefa9
Shentai <97135274+foxshakeitoff@users.noreply.github.com>
2023-01-29 09:20:21 -0500

    semi-final

---
+++ code/data_preparation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib as plt
+
+def currency_to_int(x):
+    x.replace('$','')
+    x.replace(',','')
+    x=int(x)
+    return x
+
+def get_name_bar(x):
+    df_top_20_x = df_final_aftergroupby[df_final_aftergroupby.category==x][:20]
+    df_top_20_x_name = df_top_20_x.merge(df_final)
+    df_top_20_x_name[['primary_name','ROI']].drop_duplicates().plot(kind='bar',x='primary_name',xlabel='Name', title=f'ROI by {x}');
